b706baf626c9a8cd1b2289a7adc39bed593a7b7d
okumura <a-okumura@fabrica-com.co.jp>
2026-01-16 17:13:26 +0900

    デザインシステム構築にともなうファイル変更

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,6 @@
 import type { Preview } from '@storybook/nextjs-vite'
 
-import "../src/app/variables.css"
-import "../src/app/globals.css"
+import "../src/styles/globals.css"
 
 const preview: Preview = {
   parameters: {
